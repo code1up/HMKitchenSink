e1f112849acd7a11935cad103813596157ebeef6
Alan Gorton <dev.code1up@googlemail.com>
2012-09-08 10:53:57 +0100

    Initial Commit

---
+++ HMKitchenSink/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HMKitchenSink
+//
+//  Created by Alan Gorton on 08/09/2012.
+//  Copyright (c) 2012 Alan Gorton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
